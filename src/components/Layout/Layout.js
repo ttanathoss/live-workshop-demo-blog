45c0d74626f214bbfa16b0377c624d0338b11022
Janusz Paprzycki <janusz.paprzycki@rb.com>
2021-03-03 21:02:37 +0100

    Fetching some metadata from config via graphql

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 import { main, title, navBar } from './layout.module.css';
 
-const Layout = ({ pageTitle, children }) => (
-  <main className={main}>
-    <nav className={navBar}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-    <title>{pageTitle}</title>
-    <h1 className={title}>{pageTitle}</h1>
-    {children}
-  </main>
-);
+const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `);
+
+  return (
+    <main className={main}>
+      <p>{data.site.siteMetadata.title}</p>
+      <nav className={navBar}>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <title>{pageTitle}</title>
+      <h1 className={title}>{pageTitle}</h1>
+      {children}
+    </main>
+  );
+};
 
 export default Layout;
