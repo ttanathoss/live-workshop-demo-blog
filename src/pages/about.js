05dcb43d445f3321432d7379cc0fb7ca5edf44f9
Janusz Paprzycki <janusz.paprzycki@rb.com>
2021-03-03 19:28:14 +0100

    Improved 'a' to 'Link'

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-const About = () => <h1>Hi! Welcome to my first gatsby page!</h1>;
+const About = () => (
+  <main>
+    <h1>Hi! Welcome to my first gatsby page!</h1>
+    <p>
+      <Link to="/">How about going back to home?</Link>
+    </p>
+  </main>
+);
 
 export default About;
